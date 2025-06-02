a42e2d78e15bad70356f0d001def5477ff19176f
sangb.han <sangb.han@snuailab.ai>
2025-06-02 23:28:41 +0900

    feature: scafoldding

---
+++ employees/src/main/java/kr/nowsys/employees/EmployeesApplication.java
@@ -0,0 +1,13 @@
+package kr.nowsys.employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesApplication.class, args);
+	}
+
+}
---
+++ employees/src/test/java/kr/nowsys/employees/EmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.nowsys.employees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
