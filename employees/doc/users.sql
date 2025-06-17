d66cd6e61dac8268e8f22d26c7104e2f1f0b18d3
sangb.han <sangb.han@snuailab.ai>
2025-06-17 23:39:09 +0900

    feature: employeed final

--- employees/doc/users.sql
+++ employees/doc/users.sql
@@ -28,3 +28,33 @@ INSERT INTO users (username, password, enabled) VALUES
                                                     ('user1', '{bcrypt}$2a$10$.p5wt3M60gCzgYfh.tfhj.Cee7PheDYXarGh94Uz2oOSPG2cHrXvG', true),
                                                     ('user2', '{bcrypt}$2a$10$Uwop4YYoOT0I/TWB8v9Z2uuyRg3MyGRgt.0qxv6NT5Bsn4kJ8FpM2', true),
                                                     ('admin', '{bcrypt}$2a$10$aL/ojUEqaLhV3.X4ucDPy.EX0o1Q5ZG4tMilhaJjyWcHF2jHI9KvC', true);
+
+
+-- Custom SQL Table
+CREATE TABLE members (
+                       user_id VARCHAR(50) NOT NULL PRIMARY KEY,
+                       pw VARCHAR(500) NOT NULL,
+                       active BOOLEAN NOT NULL
+);
+
+CREATE TABLE roles (
+                             user_id VARCHAR(50) NOT NULL,
+                             role VARCHAR(50) NOT NULL,
+                             CONSTRAINT fk_authorities_users FOREIGN KEY(user_id) REFERENCES members(user_id)
+);
+CREATE UNIQUE INDEX ix_auth_user_id on roles (user_id,role);
+
+INSERT INTO members VALUES
+                      ('user1', '{bcrypt}$2a$10$.p5wt3M60gCzgYfh.tfhj.Cee7PheDYXarGh94Uz2oOSPG2cHrXvG', true),
+                      ('user2', '{bcrypt}$2a$10$Uwop4YYoOT0I/TWB8v9Z2uuyRg3MyGRgt.0qxv6NT5Bsn4kJ8FpM2', true),
+                      ('admin', '{bcrypt}$2a$10$aL/ojUEqaLhV3.X4ucDPy.EX0o1Q5ZG4tMilhaJjyWcHF2jHI9KvC', true);
+
+
+INSERT INTO roles VALUES
+    ('user1', 'ROLE_EMPLOYEE'),
+    ('user2', 'ROLE_EMPLOYEE'),
+    ('user2', 'ROLE_MANAGER'),
+    ('admin', 'ROLE_EMPLOYEE'),
+    ('admin', 'ROLE_MANAGER'),
+    ('admin', 'ROLE_ADMIN');
+
